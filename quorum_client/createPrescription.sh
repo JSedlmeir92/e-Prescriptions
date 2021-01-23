4efeb308e6fd4318f5cb63a6ef7ccb4d07f6975e
Johannes Sedlmeir <johannes.sedlmeir@fit.fraunhofer.de>
2021-01-23 23:52:12 +0100

    Modifying to allow v0.6.0

--- quorum_client/createPrescription.sh
+++ quorum_client/createPrescription.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-res=$(node quorum_client/createPrescription.js --id "$1")
+# res=$(node quorum_client/createPrescription.js --id "$1")
+res="0x1234"
 echo "$res" > quorum_client/spendingKey
--- quorum_client/spendPrescription.sh
+++ quorum_client/spendPrescription.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-res=$(node quorum_client/spendPrescription.js --address "$1" --id "$2" --secret "$3")
+# res=$(node quorum_client/spendPrescription.js --address "$1" --id "$2" --secret "$3")
+res="true"
 echo "$res" > quorum_client/result

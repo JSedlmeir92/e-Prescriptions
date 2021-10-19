e90befcd92123e95e0369349e76516686ea4353e
Felix <felix@paetzold.me>
2021-10-19 19:22:46 +0200

    Updating to internal Docker-IP

--- quorum_client/truffle-config.js
+++ quorum_client/truffle-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   networks: {
     node0: {
-      host: "localhost",
-      port: 22000,
+      host: "172.16.239.11",
+      port: 8545,
       network_id: "*",
       gasPrice: 0,
       gas: 300000000,
